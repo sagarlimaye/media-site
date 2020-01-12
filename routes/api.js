698af8e9b3d96d5e7bb4fb5f261e66eb550a9387
Sagar Limaye <sagarl3232@hotmail.com>
2020-01-11 18:40:03 -0600

    Register new user route. Verify user role on POST and DELETE news route

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/auth', function(req, res, next) {
-  if(req.body && req.body.id && req.body.password) {
-    let id = req.body.id;
+  if(req.body && req.body.username && req.body.password) {
+    let id = req.body.username;
     let hash = crypto.createHash('sha256');
     hash.update(req.body.password);
     let password = hash.digest('hex');
@@ -21,8 +21,14 @@ router.post('/auth', function(req, res, next) {
   else next(createError(400));
 });
 
-router.get('/news', (req, res, next) => {
-
+router.post('register', (req, res, next) => {
+  if(req.body) {
+    let hash = crypto.createHash('sha256');
+    hash.update(req.body.password);
+    req.body.password = hash.digest('hex');
+    User.create(req.body).then(() => res.sendStatus(201)).catch(err => next(err));
+  }
+  else next(createError(400));
 });
 
 router.use(function(req, res, next) {
@@ -30,11 +36,24 @@ router.use(function(req, res, next) {
     var token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, config.secret, (err, decoded) => {
       if(err)
-        next(createError(401));
-      else next();
+        next(err);
+      else {
+        res.locals.user = decoded;
+        next();
+      }
     });
   }
   else next(createError(401));
+});
+
+router.get('/news', (req, res, next) => {
+
+});
+
+router.use((req, res, next) => {
+  if(res.locals.user && res.locals.user.role != 1)
+    next(createError(401));
+  else next();
 });
 
 router.post('/news', (req, res, next) => {
