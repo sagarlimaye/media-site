dddd44c14814ea87b125fac42d7f96fb88ceb683
Sagar Limaye <sagarl3232@hotmail.com>
2020-01-19 19:50:48 -0600

    async pipe with ngif to show log out and register links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,38 +4,33 @@ import { LoginService } from './login.service';
 import { Router } from '@angular/router';
 import { faCloud } from '@fortawesome/free-solid-svg-icons';
 import { WeatherService } from './weather.service';
+import { tap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   title = 'Media Site';
-  showLogout: boolean;
-  tokenSubscription : Subscription;
+  token$: Observable<any>
   faCloud;
   weather$: Observable<any>
+
   constructor(private loginService: LoginService, private router: Router, private weatherService: WeatherService) {
     
   }
   ngOnInit() {
-    this.tokenSubscription = this.loginService.tokenSubject.subscribe((token) => {
-      if(token)  {
-        this.showLogout = true;
-        this.weather$ = this.weatherService.getWeather('London,uk', 'b6907d289e10d714a6e88b30761fae22');
-      }
-      else {
-        this.showLogout = false;
-        this.weather$ = null;
-      } 
-    });
+    this.token$ = this.loginService.tokenSubject.pipe(tap((token) => {
+      this.weather$ = this.weatherService.getWeather('London,uk', 'b6907d289e10d714a6e88b30761fae22');
+    },
+    (err) => {
+      this.weather$ = null;
+    }));
     this.faCloud = faCloud;
   }
   logout() {
     this.loginService.logout();
     this.router.navigate(['/login']);
   }
-  ngOnDestroy() {
-    this.tokenSubscription.unsubscribe();
   }
 }
