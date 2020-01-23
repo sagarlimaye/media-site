a64c9742b42030337e2ce485e4ffb9b3a8e73c61
Sagar Limaye <sagarl3232@hotmail.com>
2020-01-23 11:02:59 -0600

    Api methods to get and update users and removed user mixin

--- java-api/src/main/java/com/mediasite/api/controllers/ApiController.java
+++ java-api/src/main/java/com/mediasite/api/controllers/ApiController.java
@@ -9,11 +9,16 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -40,7 +45,7 @@ public class ApiController {
     public Optional<News> getNewsById(@PathVariable("id") String id) {
         return newsRepository.findById(id);
     }
-    @PostMapping("/news")
+    @PostMapping("/addnews")
     public void createNews(@RequestBody News news) {
         newsRepository.save(news);
     }
@@ -48,5 +53,35 @@ public class ApiController {
     public String registerUser(@RequestBody User user) {
         user.setPassword(mSitePasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user).get_id();
+    }
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+    @GetMapping("/user/{id}")
+    public Optional<User> getUserById(@PathVariable("id") String id) {
+        return userRepository.findById(id);
+    }
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUserById(@PathVariable("id") String id, @RequestBody User user) {
+        Optional<User> u = userRepository.findById(id);
+        if(u.isPresent()) { 
+            userRepository.save(user);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        }
+        else {
+            user.set_id(null);
+            userRepository.save(user);
+            return new ResponseEntity<User>(user, HttpStatus.CREATED);
+        }
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/user/{id}")
+    public void deleteUserById(@PathVariable("id") String id) {
+        userRepository.deleteById(id);
+    }
+    @DeleteMapping("/news/{id}")
+    public void deleteNewsById(@PathVariable("id") String id) {
+        newsRepository.deleteById(id);
     }
 }
--- java-api/src/main/java/com/mediasite/api/mixins/UserMixin.java
+++
@@ -1,8 +0,0 @@
-package com.mediasite.api.mixins;
-
-public abstract class UserMixin {
-    private String _id;
-    private String username;
-    private String password;
-    private int role;
-}
