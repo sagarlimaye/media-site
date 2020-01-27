217b479f5b068f1a0c7738bf9182abd1806b1456
Sagar Limaye <sagarl3232@hotmail.com>
2020-01-27 15:29:06 -0600

    moved java api application to separate repo

--- java-api/src/main/java/com/mediasite/api/security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.mediasite.api.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "yourStrong(!)Password1";
-    public static final int EXPIRATION_TIME = 600000;
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/register";
-}
